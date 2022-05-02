93aed87868a4ecb4cf6bf2bf36b57b827618f2ed
Thiaaagao <thiaguinho.the@gmail.com>
2022-05-02 12:36:36 -0300

    Navbar Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./styles.css";
 
+import Navbar from "./components/Navbar/Navbar";
+
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <Navbar />
     </div>
   );
 }
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./navbar.css";
+
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <span> React - To do - Kanban </span>
+    </nav>
+  );
+}
